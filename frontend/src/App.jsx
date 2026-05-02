61b308c73738426652c4d174aab09aa2cadabcdf
Aanchal Verma <aanchalv201@gmail.com>
2026-05-02 14:35:58 +0530

    Updated frontend API URL for Render deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5001/api';
+const API_URL = 'https://task-manager-app-9a44.onrender.com/api';
 
 function App() {
   const [token, setToken] = useState(localStorage.getItem('token'));
